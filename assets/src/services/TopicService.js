c38cec5738000a801ee121409b77b2f19bcd6f86
zilong <jzlxiaohei@163.com>
2015-12-21 18:23:15 +0800

    format code

--- assets/src/services/TopicService.js
+++ assets/src/services/TopicService.js
@@ -4,55 +4,57 @@ import {Injectable} from 'angular2/core';
 import {Http,URLSearchParams} from 'angular2/http'
 
 @Injectable()
-export default class TopicService{
+export default class TopicService {
 
     //http:Http;
 
-    limit=40;
+    limit = 40;
 
-    page=1;
+    page = 1;
 
-    topicsSub= new Rx.ReplaySubject();
+    topicsSub = new Rx.ReplaySubject();
 
     isLoading = false;
 
     currentTopicSub = new Rx.BehaviorSubject(null)
 
 
-    constructor(http:Http){
+    constructor(http:Http) {
         this.http = http;
         this.reqNextPage()
     }
 
-    reqNextPage(){
+    reqNextPage() {
         this.isLoading = true;
 
-        var search =new URLSearchParams()
-        search.append('page',this.page)
-        search.append('limit',this.limit)
-        this.http.get('/api/v1/topics',{
-            search:search
-        })
+        var search = new URLSearchParams()
+        search.append('page', this.page)
+        search.append('limit', this.limit)
+        this.http.get('/api/v1/topics', {
+                search: search
+            })
             .map(e=>e.json())
             .map(e=>e['data'])
-            .do(()=>{this.page++})
-            .subscribe(item=>{
+            .do(()=> {
+                this.page++
+            })
+            .subscribe(item=> {
                 this.topicsSub.next(item)
-                if(this.currentTopicSub.value==null){
+                if (this.currentTopicSub.value == null) {
                     this.currentTopicSub.next(item[0])
                 }
             })
     }
 
-    setCurrentTopic(topic){
+    setCurrentTopic(topic) {
         this.currentTopicSub.next(topic)
     }
 
-    getTopicsSub(){
+    getTopicsSub() {
         return this.topicsSub;
     }
 
-    getTopicById(){
+    getTopicById() {
         return this.currentTopicSub
     }
 
